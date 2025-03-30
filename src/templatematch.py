1ce0f7c50dc6e5a8b81deaf7984ef7b686a18299
Rohit Kumar <sahil15rohit88@gmail.com>
2025-03-30 15:36:50 +0530

    Added image rotation and scale code

--- src/templatematch.py
+++ src/templatematch.py
@@ -9,6 +9,29 @@ class TemplateMatching():
     def __init__(self, config):
         self.config = config
 
+    def rotate_image(self, image, angle):
+        image_center = tuple(np.array(image.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, -angle, 1.0)
+        result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        return result
+    
+    def scale_image(self, image, percent, maxwh):
+        max_width = maxwh[1]
+        max_height = maxwh[0]
+        max_percent_width = max_width / image.shape[1] * 100
+        max_percent_height = max_height / image.shape[0] * 100
+        max_percent = 0
+        if max_percent_width < max_percent_height:
+            max_percent = max_percent_width
+        else:
+            max_percent = max_percent_height
+        if percent > max_percent:
+            percent = max_percent
+        width = int(image.shape[1] * percent / 100)
+        height = int(image.shape[0] * percent / 100)
+        result = cv2.resize(image, (width, height), interpolation = cv2.INTER_AREA)
+        return result, percent
+    
     def __call__(self, image, template):
         pass
 
